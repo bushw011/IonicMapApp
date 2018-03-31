0b159a1d2f30fa7577ed19fa49c9a7635dfc8308
Nick Bushway <bushw011@morris.umn.edu>
2018-03-31 17:46:24 -0500

    Set up mock locations

--- src/pages/locations/locations.spec.ts
+++ src/pages/locations/locations.spec.ts
@@ -4,8 +4,54 @@ import {LocationsPage} from "./locations";
 import {LocationService} from "../../app/location/location.service";
 import {FormsModule} from "@angular/forms";
 import {} from 'jasmine';
+import {Observable} from "rxjs/Observable";
 
 
 describe("Location List", () => {
+
+  let locationList: LocationsPage;
+  let fixture: ComponentFixture<LocationsPage>;
+
+  let locationServiceStub: {
+    getLocations: () => Observable<Location[]>
+  };
+
+  beforeEach(async(() => {
+    // stub of mock locations
+    locationServiceStub = {
+      getLocations: () => Observable.of([
+        {
+          ID: 'auto_place1',
+          name: 'Auto Insurance Place',
+          category: 'Auto',
+          description: 'This is an auto insurance location'
+        },
+        {
+          ID: 'home_place1',
+          name: 'Home Insurance Place',
+          category: 'Home',
+          description: 'This is a home insurance location'
+        },
+        {
+          ID: 'home&auto_place1',
+          name: 'Home & Auto Insurance Place',
+          category: 'Home & Auto',
+          description: 'This is a home & auto insurance location'
+        }
+      ])
+    };
+
+
+
+    TestBed.configureTestingModule({
+      declarations: [LocationsPage],
+      imports: [FormsModule],
+      providers: [{provide: LocationService, useValue: locationServiceStub}]
+    })
+
+
+  }));
+
+
 
 });
