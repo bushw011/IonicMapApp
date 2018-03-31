b985c4e5b191bfe612ee582f3d27f7c59fe7c1be
Nick Bushway <bushw011@morris.umn.edu>
2018-03-31 17:30:14 -0500

    Begin setting up testing

---
+++ src/pages/locations/locations.spec.ts
@@ -0,0 +1,11 @@
+import {ComponentFixture, TestBed, async} from "@angular/core/testing";
+import {Location} from "../../app/location/location";
+import {LocationsPage} from "./locations";
+import {LocationService} from "../../app/location/location.service";
+import {FormsModule} from "@angular/forms";
+import {} from 'jasmine';
+
+
+describe("Location List", () => {
+
+});
